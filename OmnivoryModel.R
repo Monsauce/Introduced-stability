2dc08c417a203a78089b9cd5c21dd0a76dad357b
Monica Granados <monica.granados@alumni.utoronto.ca>
2015-11-17 23:00:05 -0500

    adding omnivory model

---
+++ OmnivoryModel.R
@@ -0,0 +1,59 @@
+library(deSolve)
+
+#model 
+Omnivory <- function (Time, State, Pars) {
+  with(as.list(c(State, Pars)), {
+    dR = R*r*(1-R/K)-(acr*R/(1+bcr*R))*C-(1-W)*(apr*R/(1+bpr*R+bpc*C))*P
+    dC = (acr*R/(1+bcr*R))*C-W*(apc*C/(1+bpr*R+bpc*C))*P-mc*C
+    dP = (1-W)*(apr*R/(1+bpr*R+bpc*C))*P+W*(apc*C/(1+bpr*R+bpc*C))*P-mp*P
+    return(list(c(dR, dC, dP)))
+  })
+}
+
+#Parameter Set 1 
+Pars <- c(K=1, acr=7.5, apr=0.5, apc=2, mc=0.79188, mp=0.28516, bcr=5, bpr=0.5, bpc=2, r=2.5)
+State <- c(R = 0.5, C = 0.5, P=0.5)
+Time <- seq(0, 1000, by = 1)
+
+#plot time series with parameter set 1 
+out <- as.data.frame(ode(func = Omnivory, y = State, parms = Pars, times = Time))
+
+#function to get the peaks for predator over parameter range 
+peaks <- function(x, method="max") {
+  l <- length(x)
+  xm1 <- c(x[-1], x[l])
+  xp1 <- c(x[1], x[-l])
+  if (method=="both") {
+    p <- x[x > xm1 & x > xp1 | x < xm1 & x < xp1]
+  } else {
+    p <- x[x > xm1 & x > xp1]
+  }
+  p
+}
+
+#make empty data frames for loop 
+maxpr<-NULL
+pikpr<-NULL
+
+
+#make bifurcation plot over parameter range of w (from 0 to 0.5) using Parameter Set 1
+plot(0,0, xlim=c(0, 1), ylim=c(0,1.8), type="n", xlab="Strength of omnivory", ylab="Maxima of Predator")
+for (w in seq(0,0.5,0.1)) {
+  Pars["W"] <- w
+  out <- as.data.frame(lsoda(State, Time, Omnivory, Pars))
+  
+#Remove first three fourths of time series and consider last fourth for finding the peaks
+  
+  lpr <- length(out$P) %/% 4
+  
+  outpr <- out[(3*lpr):(4*lpr),]
+  
+  maxpr <- c(maxpr, max(outpr$P))
+  
+  pikpr <- peaks((outpr$P), method="max")
+  
+  l <- length(out)
+  xstart <- c(R=out$R[l], co=out$C[l], pr=out$R[l])
+  
+  points(rep(w, length(pikpr)), pikpr, pch=".", col="black")
+}
